8cdfea7d45836ee7bb33a55005b9561c5b23a9a7
Ben Winding <hello@benwinding.com>
2020-04-02 12:43:18 +1030

    Add ls aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,17 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
+if $(command -v gls >/dev/null 2>&1)
 then
+  echo gls installed
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
+else 
+  echo gls not installed
+  # some more ls aliases
+  alias ll='ls -alF -color'
+  alias la='ls -A -color'
+  alias l='ls -CF -color'
 fi
