cb4e12092f0ba8d07aacdf603878606f03696d45
Ben Winding <hello@benwinding.com>
2020-04-06 09:18:32 +0930

    Add dart path

--- android/path.zsh
+++ android/path.zsh
@@ -6,3 +6,5 @@ export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
 # Setup flutter paths
 export PATH=${PATH}:$HOME/flutter/bin
+# Setup dart paths see: https://dart.dev/get-dart
+export PATH="$PATH:/usr/lib/dart/bin"
