a96fe452c7e3610df6558bf3ebce55b2a9f8229e
benwinding <hello@benwinding.com>
2020-04-12 18:07:02 +0930

    Fix enhance reliance on bash

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,10 +1,5 @@
 alias d='docker $*'
 alias d-c='docker-compose $*'
 
-
 alias docker-kill-all='docker kill `docker ps -aq`'
 alias docker-rm-all='docker rm `docker ps -aq`'
-
-# from https://github.com/alexjc/neural-enhance
-alias enhance='function ne() { docker run --rm -v "$(pwd)/`dirname ${@:$#}`":/ne/input -it alexjc/neural-enhance ${@:1:$#-1} "input/`basename ${@:$#}`"; }; ne'
-
