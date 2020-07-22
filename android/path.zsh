69b1a0fbc0d440442cf0affdf34f67d08d22e416
Ben Winding <hello@benwinding.com>
2020-07-22 13:16:34 +0930

    Edit paths

--- android/path.zsh
+++ android/path.zsh
@@ -3,8 +3,3 @@
 
 export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
-
-# Setup flutter paths
-export PATH=${PATH}:$HOME/flutter/bin
-# Setup dart paths see: https://dart.dev/get-dart
-export PATH="$PATH:/usr/lib/dart/bin"
--- system/_path.zsh
+++ system/_path.zsh
@@ -2,3 +2,5 @@ export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export PATH=$PATH:~/.local/bin
 export PATH=$PATH:~/anaconda3/bin
+export PATH=$PATH:~/programs
+export PATH=$PATH:~/packages
