22579c17ea8877dcd4231cb414ef210850e50a98
Ben Winding <hello@benwinding.com>
2020-09-04 16:44:12 +0930

    Added ruby paths

--- android/path.zsh
+++ android/path.zsh
@@ -3,3 +3,5 @@
 
 export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+export PATH=${PATH}:$ANDROID_HOME/build-tools/28.0.3/
+export PATH=${PATH}:/home/linuxbrew/.linuxbrew/lib/ruby/gems/2.7.0/bin
