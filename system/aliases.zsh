b6b75b73897b3febfd3ed532ee0f30bc46a1d203
Ben Winding <hello@benwinding.com>
2020-09-28 13:04:34 +0930

    Add default current directory to a few features "./"

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -65,7 +65,7 @@ alias l='ls -CF'
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 alias o='xdg-open'
-alias files='nautilus'
+alias files='nautilus ./'
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
 alias ys='yarn start'
