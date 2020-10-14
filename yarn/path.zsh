0a92396cd54a682ed793e4e78026b3a8e01559b7
Ben Winding <hello@benwinding.com>
2020-10-15 09:31:23 +1030

    Better lazy loading of nvm

--- yarn/path.zsh
+++ yarn/path.zsh
@@ -1,13 +1,5 @@
 # sup yarn
 # https://yarnpkg.com
 
-# Load NVM
-export NVM_DIR="$HOME/.nvm"
-[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh" --no-use
-
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # Add yarn to path
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
