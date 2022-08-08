329a101bc210951ca4ca26ec5014547c34d0bf29
Ben Winding <benw@canva.com>
2022-08-08 10:52:37 +1000

    Better git alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,8 +7,8 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl1="git log -1 --stat --oneline"
 alias gp='git push origin HEAD'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
