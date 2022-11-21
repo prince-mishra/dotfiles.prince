bd530801973b9032f3c13cfdefa8115968c04bd6
Ben Winding <hello@benwinding.com>
2022-11-21 18:41:19 +1030

    Better up/down history searching

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,13 +3,3 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
-
-# OS specific
-case "$OSTYPE" in
-# Linux
-linux*)
-  # make the up and down arrows cycle through commands that match the start of a line
-  # Requires installation via: git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-  source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-  ;;
-esac
