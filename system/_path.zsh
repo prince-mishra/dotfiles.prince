826e8fb30499b32734093eebb7a8b6548e1b18b2
Ben Winding <benw@canva.com>
2024-07-03 16:11:48 +1000

    Add `~/.bin` to `PATH` _(wtf wasn't this in here)_

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,5 +1,6 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH=$PATH:~/.bin
 export PATH=$PATH:~/.local/bin
 export PATH=$PATH:~/anaconda3/bin
 export PATH=$PATH:~/programs
