a4cdad197a657f23f0c34098f3e0c0ba32b69083
Prince Mishra <indiandexter007@gmail.com>
2025-06-17 16:57:25 +0530

    Add common python utils in a dedicated env

---
+++ pyutils/aliases.zsh
@@ -0,0 +1,8 @@
+_mamba_run() {
+    micromamba run -n pyutils "$@"
+}
+
+alias visidata='_mamba_run visidata'
+alias vd='_mamba_run vd'
+alias httpie='_mamba_run httpie'
+alias pre-commit='_mamba_run pre-commit'
---
+++ pyutils/install.sh
@@ -0,0 +1,17 @@
+#docker build -t pyutils .
+micromamba create -y -n pyutils
+
+micromamba install -y -n pyutils -c conda-forge \
+    git \
+    curl \
+    wget \
+    python=3.11 \
+    pip \
+    visidata \
+    pre-commit \
+    black \
+    ruff \
+    csvkit \
+    httpie
+
+
